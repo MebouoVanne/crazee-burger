e888ef746d30a82200378f916c5a2187a53abe15
MebouoVane <2017vaness@gmail.com>
2023-08-17 23:17:16 +0200

    ✨F01 LOGIN>ajout d'un titre et d'un formulaire

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
 
 function LoginPage() {
+
+    const [inputValue, setInputValue] = useState("")
+    const handleSubmit = (event)  => { 
+        event.preventDefault();
+        alert(`bonjour ${inputValue}`)
+        setInputValue("")
+     }
+
+     const handleChange = (event) => { 
+        setInputValue(event.target.value)
+        
+      }
   return (
-    <div>LoginPage</div>
+    <div>
+      <h1> Bienvenu chez nous </h1>
+      <h2> Connectez vous</h2>
+      <form action='submit' onSubmit={handleSubmit}>
+        <input type='text' value={inputValue} required onChange={handleChange} placeholder='Entrer votre prènom ...'/>
+        <button>Accéder à votre espace </button>
+      </form>
+    
+    </div>
   )
 }
 
