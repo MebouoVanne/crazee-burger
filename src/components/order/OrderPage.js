edbd7352e65edc291474199f5f216b5956ff0a24
MebouoVane <2017vaness@gmail.com>
2023-08-22 22:11:49 +0200

    F04: ajout du navbar,main,layout

--- src/components/order/OrderPage.js
+++ src/components/order/OrderPage.js
@@ -1,20 +1,55 @@
 import React, { Fragment } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
+import styled from "styled-components";
 
-function OrderPage(props) {
+function OrderPage() {
 
  const {username}= useParams();
  console.log("username",username)
 
     return (
-        <Fragment>
-            <h1>Bonjour {username} </h1>
+        <OrderPageStyle>
+            navbar
+            <div className='container'>
+                <div className='navbar'>
+                <h1>Bonjour {username} </h1>
            <Link to="/">
               <button > Deconnexion </button>
-           </Link> 
-        </Fragment>
+           </Link>
+                </div>
+            
+            </div>
+             <div className='main'> Main </div>
+        </OrderPageStyle>
 
     )
 }
 
 export default OrderPage
+
+const OrderPageStyle = styled.div ` 
+background:orange;
+height: 100vh;
+display: flex;
+justify-content: center;
+qlign-items: center;
+
+.container {
+    background:red;
+    height: 95vh;
+    width: 1400px;
+    display: flex;
+    flex-direction:column;
+}
+
+.navbar {
+    background:blue;
+    height: 10vh;
+}
+.main {
+    background:green;
+    /* height:(95vh - 10vh);*/
+    flex:1;
+
+}
+`
