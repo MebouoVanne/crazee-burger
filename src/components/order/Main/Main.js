740ebb572fc9fe7ea0c4460fc746aa8f6807c420
MebouoVane <2017vaness@gmail.com>
2023-08-24 20:50:04 +0200

    F05: >MENU>IMAGES>

---
+++ src/components/order/Main/Main.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from "styled-components";
+import { theme } from '../../../theme';
+import Menu from './Menu';
+
+export default function Main() {
+  return (
+    <MainStyled>
+       <div className='basket'>basket</div>
+       <Menu/>
+    </MainStyled>
+  )
+}
+
+const MainStyled = styled.div `
+background:${theme.colors.background_white};
+flex:1;
+
+border-bottom-left-radius: ${theme.borderRadius.extraRound};
+border-bottom-right-radius: ${theme.borderRadius.extraRound};
+box-shadow: 0px 8px 20px 8px rgba(0,0,0,0.2) inset;
+display: grid;
+grid-template-columns:25% 1fr ;
+
+.basket {
+  background: pink;
+}
+
+`
--- src/components/order/OrderPage.js
+++ src/components/order/OrderPage.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import styled from "styled-components";
-import Navbar from './Navbar';
-import Main from './Main';
+import Navbar from './Navbar/Navbar';
+import Main from './Main/Main';
 import { theme } from '../../theme';
 
 function OrderPage() {
