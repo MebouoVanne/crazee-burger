817502a987463d2002e1c83d9407b71e4575fcd9
MebouoVane <2017vaness@gmail.com>
2023-08-26 00:29:59 +0200

    F06

--- src/components/order/Navbar/NavbarRightSide.js
+++ src/components/order/Navbar/NavbarRightSide.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from "styled-components";
 import Profile from './profile';
+import ToggleButton from '../../reusable-ui/ToggleButton';
+import ToastAdmin from './ToastAdmin';
+import { toast } from 'react-toastify';
+
 
 export default function NavbarRightSide({ username }) {
+
+   const [isModeAdmin, setIsModeAdmin] = useState(false)
+
+   const displayToastNotification = () =>{
+    if(!isModeAdmin){
+      toast.info("Mode admin activé",{
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
+    }
+    setIsModeAdmin(!isModeAdmin)
+   }
   return (
     <NavbarRightSideStyle >
-      {/* <div className='admin-button'>Admoin Button </div> */}
-
+      <ToggleButton 
+      labelIfChecked='desactiver le mode admin' 
+      labelIfUnchecked='activer le mode admin'
+      onToggle={displayToastNotification}
+      />
       <Profile username={username} />
+      <ToastAdmin/>
     </NavbarRightSideStyle>
   )
 }
